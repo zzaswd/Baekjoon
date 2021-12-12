d30e64fd567a705c137f9bfc4374e647fe134089
Kang.SH <zzaswd@naver.com>
2021-12-12 21:18:24 +0900

    체스해결.
    3번의 시도, 배열의 index 유의.

--- 1018.cpp
+++ 1018.cpp
@@ -1,13 +1,72 @@
 #include <iostream>
 
 using namespace std;
-//앙 ??
-// 
-//아 개어렵네.
-// 왜 예제 입력 4번이 출력이 31이 나오지..?
-// 알았다. 나는 좌측 상단 모서리를 조건으로 나눴었는데
-// 우측하단이 W가 되면서 오히려 좌측 상단을 W로 바꾸는 것이 더 적은 횟수.
-// 따라서 조건을 만족하지 못한 것.
+
+//세번째시도. 도저히 안풀려서 블로그를 참고했음.
+// 기준은 나와 비슷한데, 이미 char array을 선언하여 차이의 개수를 구했음.
+
+
+char black_first[8][8] = {
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'} };
+
+char white_first[8][8] = {
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'},
+	{'W','B','W','B','W','B','W','B'},
+	{'B','W','B','W','B','W','B','W'} };
+
+
+int main() {
+	int M, N;
+	int Min = 2000;
+
+	cin >> M >> N;
+
+	char arr[50][50];
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> arr[j][i];
+		}
+	}
+
+	for (int s_x = 0; s_x <= M - 8; s_x++) {
+		for (int s_y = 0; s_y <= N - 8; s_y++) {
+			int count_W = 0;
+			int count_B = 0;
+			for(int i = s_x; i < s_x + 8; i++) {
+				for (int j = s_y; j < s_y+8; j++) {
+					if (arr[j][i] != black_first[j-s_y][i-s_x]) 	count_B++; // 이 부분에서 시간이 조금 걸렸는데 
+					if (arr[j][i] != white_first[j-s_y][i-s_x]) 	count_W++; // black_first와 white_first의 Index에는 꼭 j-s_y, i-s_x 가 들어가야 함.
+				}															   // 똑같이 j와 i가 들어간다면 8*8 배열인 black,white 범위를 벗어나게 됌.
+			}
+			Min = min(Min, min(count_B, count_W));
+		}
+	}
+	
+	cout << Min;
+}
+
+
+/* 두번째 시도.
+이전 시도와 다르게, 8*8 첫글자의 글자를 확인하고 그에 해당하는 조건들을 부여.
+대부분의 입력들은 원하는대로 출력이 나왔음.
+
+왜 예제 입력 4번이 출력이 31이 나오지.. ?
+알았다. 나는 좌측 상단 모서리를 조건으로 나눴었는데
+ 우측하단이 W가 되면서 오히려 좌측 상단을 W로 바꾸는 것이 더 적은 횟수.
+ 따라서 조건을 만족하지 못한 것.
 
 
 int main() {
@@ -87,7 +146,7 @@ int main() {
 
 	cout << Min;
 }
-
+*/
 
 
 /* 첫번째 시도.
